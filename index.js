5d87f4cb1ae5f0fa238fd84e23d011a0570e7265
Vetle Engesbak <vetle.engesbak@gmail.com>
2019-02-10 08:53:29 +0100

    trying to find the heroku port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express();
+const herokuPort = process.env.PORT;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('Listening on port: '  + heorkuPort + ".")
 });
