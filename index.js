50ad060e9821c4c337fc840dab9885546cfb3cb1
Vetle Engesbak <vetle.engesbak@gmail.com>
2019-02-10 08:58:34 +0100

    trying to find the heroku port

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Listening on port: '  + herokuPort + ".")
 });
