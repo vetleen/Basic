f6727acc055144085e70b47be018678024693815
Vetle Engesbak <vetle.engesbak@gmail.com>
2019-02-16 09:27:23 +0100

    ...

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const herokuPort = process.env.PORT;
 // Connect to database
 var mongoose = require('mongoose');
 console.log("trying to connect to: " + process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
