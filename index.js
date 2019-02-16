8be3d4814cb7fabec1a62b836ea05fe7d864a76c
Vetle Engesbak <vetle.engesbak@gmail.com>
2019-02-16 09:24:44 +0100

    begginning to mess wioth DB

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express();
 const herokuPort = process.env.PORT;
 
+// Connect to database
+var mongoose = require('mongoose');
+console.log("trying to connect to: " + process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("DB successfully connected!");
+});
 
+
+// App
 app.get('/', (req, res) => {
   res.send('Hello other world!')
 });
