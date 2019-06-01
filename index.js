90cf9fc400ca4c041ca8f97a0f62d9a9a86dd8eb
Vetle Engesbak <vetle.engesbak@gmail.com>
2019-06-01 10:40:44 +0200

    maybe local db works now?

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+console.log (process.env.MONGODB_URI); //:27017
+console.log (process.env.PORT);
+
 const express = require('express')
 const app = express();
 const herokuPort = process.env.PORT;
@@ -5,7 +8,7 @@ const herokuPort = process.env.PORT;
 // Connect to database
 var mongoose = require('mongoose');
 //console.log("trying to connect to: " + process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/myappdb", { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
